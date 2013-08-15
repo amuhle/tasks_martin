4ef8195e531f7120fbb6978fd2f3c6a1c3dc5265
Martin F <amuhlethaler@vairix.com>
2013-08-15 18:37:19 -0300

    #1 change the type of phone from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815202823) do
+ActiveRecord::Schema.define(:version => 20130815213452) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "role"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
