63d050921dd3d87b653ed7fba6c5a9d5331d89b2
Martin F <amuhlethaler@vairix.com>
2013-08-15 18:05:14 -0300

    commit with code in english

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def index
+  	@users = User.all
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def show
+  end
+
+  def create
+  	@user = User.create(params[:user])
+  	redirect_to users_path
+  end
+
+  def update
+  	@user = User.find(params[:id])
+	@user.update_attributes(params[:user])
+	redirect_to users_path
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '69b22641c1d8048ae1132236c6fb6349dc6a89712fc43502a18010b991e0e7c911aa5f4e684e70b2914afaf5ed8983de6f53dca7e641dee0ca2886c8bbde81fd'
